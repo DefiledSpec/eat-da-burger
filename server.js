de1bdea02f941fc8e1ea7660409213768723b383
DefiledSpec <defiledspec@gmail.com>
2018-08-09 23:26:16 -0500

    setup server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+const hbs = require('express-handlebars')
+const burgersController = require('./controllers/burgersController')
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+// Setup handlebars
+app.engine('hbs', hbs({ defaultLayout: 'main', extname: 'hbs' }))
+app.use('view engine', 'hbs')
+
+// Setup Morgan logger
+app.use(logger('dev'))
+
+// Setup Body Parser
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// Routes
+app.use('/', burgersController)
+
+// Start Server listening on (PORT)
+app.listen(PORT, () => console.log(`App listening on http://localhost:${PORT}`))
